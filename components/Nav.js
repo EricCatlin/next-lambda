c9bee67eb990ff0ca5a356160634772950709cd1
Eric Catlin <eroccatlun@gmail.com>
2020-02-18 09:45:36 -0800

    Remove node_modules and .next

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,61 @@
 import Link from "next/link";
 
 const Nav = () => (
-  <ul style={{flexBasis:'row', display:'flex', justifyContent:'space-around', backgroundColor:'lightgrey'}}>
-    <li>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/ui/mock/sync">
-        <a>Sync</a>
-      </Link>
-    </li>
-  </ul>
+  <>
+    <ul style={{ flexBasis: 'row', display: 'flex', justifyContent: 'space-around', backgroundColor: 'lightgrey' }}>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/ui/rendered/sync">
+          <a>Sync</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/ui/rendered/async">
+          <a>Async</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/ui/rendered/dynamicWHAT">
+          <a>Dynamic</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/ui/rendered/dynamic/WHAT/sub">
+          <a>Dynamic, Terminating</a>
+        </Link>
+      </li>
+    </ul>
+    <ul style={{ flexBasis: 'row', display: 'flex', justifyContent: 'space-around', backgroundColor: 'lightgrey' }}>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/ui/swr/sync">
+          <a>Sync</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/ui/swr/async">
+          <a>Async</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/ui/swr/dynamicWHAT">
+          <a>Dynamic</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/ui/swr/dynamic/WHAT/sub">
+          <a>Dynamic, Terminating</a>
+        </Link>
+      </li>
+    </ul>
+  </>
 );
-export {Nav}
+export { Nav }
